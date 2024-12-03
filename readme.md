bf97160074e4c9555ed088e1fe30be9b98249653
stuckinthespiderweb <tim.destexhe@icloud.com>
2024-12-03 08:51:33 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Arrays in JS
+
+Dit zijn enkele oefeningen op arrays. Ze horen bij de twee hoofdstukjes Arrays in de cursussite. Je vult telkens aan onder elke stukje commentaar in student.js. Aan bod komen:
+
+- array maken en vullen;
+- elementen toevoegen of verwijderen;
+- zoeken in een array;
+- een functie schrijven die iets met de elementen van een array doet.
---
+++ student.js
@@ -0,0 +1,30 @@
+// Maak een array fruit met vier verschillende fruitsoorten 'appel', 'banaan', 'kiwi', 'peer'.
+const fruit = ;
+
+// merk op dat je hier een const kan gebruiken als je de array zelf niet verandert, maar enkel elementen wil toevoegen of verwijderen. Dat kan dus wel in een const.
+
+// Toon de array fruit in de console. 
+
+
+// Voeg nu achteraan in de array een vijfde fruitsoort toe 'sinaasappel'.
+
+
+// Voeg vooraan in de array een zesde fruitsoort toe 'ananas'.
+
+
+// verwijder de laatste fruitsoort uit de array.
+
+
+// toon in de console het aantal elementen dat er nu in de array fruit zit. Schrijf een zin als volgt: "De array fruit bevat 5 elementen."
+
+
+// Bevat de array fruit de fruitsoort 'kiwi'? Gebruik de includes methode. Vervolledig onderstaande regel code.
+console.log(`Komt kiwi voor: ${} `);
+
+// Zoek de index op van de fruitsoort 'banaan' in de array fruit. Toon het resultaat in de console.
+
+
+// Laten we wat gaan programmeren met de array fruit. Schrijf een functie 'langste' die de langste fruitsoort (die met het meeste letters) uit de array fruit teruggeeft. Zorg dat je functie ook werkt voor andere arrays met strings.
+
+
+console.log(`Het fruit met het meeste letters is ${langste(fruit)}`);
